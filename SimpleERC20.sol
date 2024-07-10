13d0363b21ceb8bf010884b128331ecccf3d9a06
Aditya Negi <119617732+Cyrus748@users.noreply.github.com>
2024-07-10 22:18:57 +0530

    Update SimpleERC20.sol

--- SimpleERC20.sol
+++ SimpleERC20.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleToken is ERC20 {
-    constructor() ERC20("AdityaToken", "ADT") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
     }
 }
